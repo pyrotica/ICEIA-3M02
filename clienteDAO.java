43d3cc0e50c132e01bfada4977dcb971a7aa7e57
Miguel <perfectact.2023@gmail.com>
2025-10-03 11:26:38 -0300

    Create clienteDAO.java

---
+++ clienteDAO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miguesteste;
+
+/**
+ *
+ * @author user
+ */
+public class clienteDAO {
+    
+    
+    public void cadastrarcliente(){
+        
+    }
+    public void atualizarcliente(){
+        
+    }
+    public void deletarcliente(){
+        
+    }
+    public void buscarcliente(){
+        
+    }
+    public void listarcliente(){
+        
+        
+    }
+}
