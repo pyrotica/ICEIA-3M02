1852c11caa047c0612bf16e95b1c1939ba5ebf8d
Miguel <perfectact.2023@gmail.com>
2025-10-03 11:25:52 -0300

    Create pedido.java

---
+++ pedido.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miguesteste;
+
+/**
+ *
+ * @author user
+ */
+public class pedido {
+    private int cod;
+    private cliente cliente;
+
+    /**
+     * @return the cod
+     */
+    public int getCod() {
+        return cod;
+    }
+
+    /**
+     * @param cod the cod to set
+     */
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    /**
+     * @return the cliente
+     */
+    public cliente getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(cliente cliente) {
+        this.cliente = cliente;
+    }
+}
