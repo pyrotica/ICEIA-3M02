8003022319673dadb6656a54ef9eaab06d12ec8b
Miguel <perfectact.2023@gmail.com>
2025-10-03 11:24:52 -0300

    Create pizza.java

---
+++ pizza.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miguesteste;
+
+/**
+ *
+ * @author user
+ */
+public class pizza {
+    private int cod;
+    private string sabor;
+    private string descricao;
+    private double preco;
+
+    /**
+     * @return the cod
+     */
+    public int getCod() {
+        return cod;
+    }
+
+    /**
+     * @param cod the cod to set
+     */
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    /**
+     * @return the sabor
+     */
+    public string getSabor() {
+        return sabor;
+    }
+
+    /**
+     * @param sabor the sabor to set
+     */
+    public void setSabor(string sabor) {
+        this.sabor = sabor;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public string getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(string descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the preco
+     */
+    public double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
+
+
