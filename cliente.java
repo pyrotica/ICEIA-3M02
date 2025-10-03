812daa68f3deae7ae95459929780fa33a2b241e8
Miguel <perfectact.2023@gmail.com>
2025-10-03 11:24:13 -0300

    Create cliente.java

---
+++ cliente.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miguesteste;
+
+public class cliente{
+    private int cod;
+    private string nome;
+    private string endereco;
+    private string telefone;
+
+    /**
+     * @return the cod
+     */
+    public int getCod() {
+        return cod;
+    }
+
+    /**
+     * @param cod the cod to set
+     */
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    /**
+     * @return the nome
+     */
+    public string getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public string getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(string endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public string getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(string telefone) {
+        this.telefone = telefone;
+    }
+}
+
